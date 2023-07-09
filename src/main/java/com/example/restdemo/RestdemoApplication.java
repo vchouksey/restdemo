4674593abab2e12dc2980821e70969bcd1a83cdf
Vishal <vkchouksey14@gmail.com>
2023-07-09 16:51:07 +0100

    next commit

--- src/main/java/com/example/restdemo/RestdemoApplication.java
+++ src/main/java/com/example/restdemo/RestdemoApplication.java
@@ -2,12 +2,20 @@ package com.example.restdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RestdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestdemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String getMessage() {
+		return "HelloWorld";
 	}
 
 }
