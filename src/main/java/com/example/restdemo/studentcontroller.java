84ef460c049fe869ab1de202dbcb29ad15a02ce8
Vishal <vkchouksey14@gmail.com>
2023-07-09 16:33:09 +0100

    final commit

--- src/main/java/com/example/restdemo/studentcontroller.java
+++ src/main/java/com/example/restdemo/studentcontroller.java
@@ -1,2 +1,16 @@
-package com.example.restdemo;public class studentcontroller {
+package com.example.restdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class StudentController {
+
+    public StudentController() {
+    }
+
+    @GetMapping("/")
+    public String all() {
+        return "HelloWorld";
+    }
 }
